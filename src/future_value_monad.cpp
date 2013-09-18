b2d3d038fc561a9ad2b5212882a10ba3bf3e6f6c
fons haffmans <fons.haffmans@gmail.com>
2013-09-18 06:12:06 -0400

    added

---
+++ src/future_value_monad.cpp
@@ -0,0 +1,5 @@
+#include "proto.hpp"
+#include "show.hpp"
+#include "either_monad.hpp"
+#include "curry.hpp"
+#include "bracket.hpp"
---
+++ src/future_value_monad.hpp
@@ -0,0 +1,11 @@
+#ifndef h__future_value_monad__h
+#define h__future_value_monad__h
+
+#include "future_value.hpp"
+#include "functor.hpp"
+#include "applicative_functor.hpp"
+#include "monad.hpp"
+
+
+
+#endif
